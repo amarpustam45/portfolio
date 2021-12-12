57e6557a306b11514afc76beaa5fc4fbb2a0b7b4
amarpustam45 <amarpustam@hotmail.com>
2021-12-11 23:19:18 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Work from './components/Work';
+import Contact from './components/Contact';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/work' element={<Work />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route path='*' element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>home</Link>
+        </li>
+        <li>
+          <Link to='/about'>about</Link>
+        </li>
+        <li>
+          <Link to='/work'>my work</Link>
+        </li>
+        <li>
+          <Link to='/contact'>contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
