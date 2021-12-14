ce7147280d9b465558822dbb73faa5243b7be0ee
amarpustam45 <amarpustam@hotmail.com>
2021-12-13 22:50:48 -0400

    home context added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import '../styles/Home.css';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <section className='home-container'>
+      <div className='banner'>welcome</div>
+      <section className='intro'>
+        <p>Hi there,</p>
+        <div className='about-link-container'>
+          <p>I am</p>
+          <NavLink exact to='/about' className='to-about'>
+            Amar Pustam
+          </NavLink>
+        </div>
+        <p>front-end ui/ux designer</p>
+      </section>
+    </section>
+  );
 };
 
 export default Home;
