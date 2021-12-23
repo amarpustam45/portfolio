a4d6937b2b57f52344d3150f8756698afb62642e
amarpustam45 <amarpustam@hotmail.com>
2021-12-22 22:52:47 -0400

    almost completed just need mobile response

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,34 +1,80 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import data from './data';
+import { NavLink } from 'react-router-dom';
 import { FaAngleDoubleRight, FaAngleDoubleLeft } from 'react-icons/fa';
 import '../styles/Work.css';
+import { BsBoxArrowInUpRight } from 'react-icons/bs';
 
 const Work = () => {
   const [workData, setWorkData] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = workData.length - 1;
+    if (index < 0) setIndex(lastIndex);
+    if (index > lastIndex) setIndex(0);
+  }, [index, workData]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 8000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
 
   return (
     <section className='work-container'>
       <div className='banner work-banner'>my work</div>
-      <section className='work-center'>
-        {workData.map((site, siteIndex) => {
-          const { id, siteName, image, url, description, techDesc } = site;
-          console.log(image);
-          return (
-            <article key={id}>
-              <a href={url} target='_blank'>
-                <img src={image} alt={siteName} />
-              </a>
-              <p>{siteName}</p>
-            </article>
-          );
-        })}
+      <section className='all-content'>
+        <section className='main-content'>
+          {workData.map((site, siteIndex) => {
+            const { id, siteName, image, url, description, techDesc } = site;
+
+            let position = 'next-slide';
+            if (siteIndex === index) position = 'active-slide';
+            if (
+              siteIndex === index - 1 ||
+              (index === 0 && siteIndex === workData.length - 1)
+            )
+              position = 'last-slide';
+
+            return (
+              <article key={id} className={`card-control ${position}`}>
+                <section className='contents'>
+                  <img src={image} alt={siteName} />
+                  <section className='card-info-container'>
+                    <p className='site-name'>{siteName}</p>
+                    <p className='site-desc'>{description}</p>
+                    <p className='tech-desc'>{techDesc}</p>
+                    <a href={url} target='_blank' className='link-to-site'>
+                      <div className='nav-item nav-item-link'>
+                        <BsBoxArrowInUpRight className='new-tab-icon' />
+                        take me there
+                      </div>
+                    </a>
+                  </section>
+                </section>
+              </article>
+            );
+          })}
+        </section>
+        <p className='call-to-action'>
+          Like what you see? Get in{' '}
+          <NavLink exact to='/contact' className='to-contact nav-item'>
+            touch here
+          </NavLink>
+          or send an email:
+          <span className='email'>amarpustam@hotmail.com</span>
+        </p>
+        <button className='prev' onClick={() => setIndex(index - 1)}>
+          <FaAngleDoubleLeft />
+        </button>
+        <button className='next' onClick={() => setIndex(index + 1)}>
+          <FaAngleDoubleRight />
+        </button>
       </section>
-      <button className='prev'>
-        <FaAngleDoubleLeft />
-      </button>
-      <button className='next'>
-        <FaAngleDoubleRight />
-      </button>
     </section>
   );
 };
--- src/components/data.js
+++ src/components/data.js
@@ -1,4 +1,8 @@
 import { v4 as uuidv4 } from 'uuid';
+import portfolio from '../images/portfolio.png';
+import restaurant from '../images/restaurant.png';
+import simpledraw from '../images/simple-draw.png';
+import todo from '../images/todo.png';
 
 const sites = [
   {
@@ -7,7 +11,7 @@ const sites = [
     url: 'https://amar-pustam.netlify.app',
     description: 'This is my portfolio',
     techDesc: 'Built using react',
-    image: '../images/portfolio.png',
+    image: portfolio,
   },
   {
     id: uuidv4(),
@@ -15,7 +19,7 @@ const sites = [
     url: 'https://restaurant-demo-amarpustam.netlify.app',
     description: 'This is a simple demo of a restaurant menu application',
     techDesc: 'This was built using vanilla html,css and javascript',
-    image: '../images/restaurant.png',
+    image: restaurant,
   },
   {
     id: uuidv4(),
@@ -24,8 +28,8 @@ const sites = [
     description:
       'This site allows the user to create drawings in the web browser and download the result.',
     techDesc:
-      'This site was build using b=vanilla html, css and javascript. The canvas tag was used mainly to achieve the functionality of this site.',
-    images: '../images/simpleDraw.png',
+      'This site was build using vanilla html, css and javascript. The canvas tag was used mainly to achieve the functionality of this site.',
+    image: simpledraw,
   },
   {
     id: uuidv4(),
@@ -34,7 +38,7 @@ const sites = [
     description:
       "This is a to do app build in react. it's purpose of this app is to demonstrate the basic use of react hooks",
     techDesc: 'This application was built using react.',
-    image: '../images/todo.png',
+    image: todo,
   },
 ];
 
