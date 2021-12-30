3455add579ebecef01288e82a9ef71801dfa8f88
amarpustam45 <amarpustam@hotmail.com>
2021-12-30 15:21:35 -0400

    start of the mobile response

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,47 +1,63 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-      <ul className='nav-pages'>
-        <li>
-          <NavLink exact to='/' className='nav-links' activeClassName='active'>
-            home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to='/about' className='nav-links'>
-            about
-          </NavLink>
-        </li>
-        <li>
-          <NavLink exact to='/work' className='nav-links'>
-            my work
-          </NavLink>
-        </li>
-      </ul>
-      <ul className='nav-contact'>
-        <li>
-          <NavLink exact to='/contact' className='nav-links nav-links-contact'>
-            contact
-          </NavLink>
-        </li>
-        <li>
-          <a href='https://github.com/amarpustam45' target='_blank'>
-            <FaGithub className='nav-icon' />
-          </a>
-        </li>
-        <li>
-          <a href='https://www.linkedin.com/in/amar-pustam' target='_blank'>
-            <FaLinkedin className='nav-icon' />
-          </a>
-        </li>
-        <li></li>
-      </ul>
-    </nav>
+    <section className='navcontainer'>
+      <span className='hamburger'>
+        <GiHamburgerMenu />
+      </span>
+      <nav className='navbar'>
+        <ul className='nav-pages'>
+          <li>
+            <NavLink
+              exact
+              to='/'
+              className='nav-links'
+              activeClassName='active'
+            >
+              home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to='/about' className='nav-links'>
+              about
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to='/work' className='nav-links'>
+              my work
+            </NavLink>
+          </li>
+        </ul>
+        <ul className='nav-contact'>
+          <li>
+            <NavLink
+              exact
+              to='/contact'
+              className='nav-links nav-links-contact'
+            >
+              contact
+            </NavLink>
+          </li>
+          <div className='icons'>
+            <li>
+              <a href='https://github.com/amarpustam45' target='_blank'>
+                <FaGithub className='nav-icon' />
+              </a>
+            </li>
+            <li>
+              <a href='https://www.linkedin.com/in/amar-pustam' target='_blank'>
+                <FaLinkedin className='nav-icon' />
+              </a>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </section>
   );
 };
 
